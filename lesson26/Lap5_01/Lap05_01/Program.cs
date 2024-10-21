33db9bd3e1f02a7afacdf6bd11d248f4527c4376
Nguyễn Văn An <vanan9524@gmail.com>
2024-10-21 11:29:54 +0700

    thứ 2 21/10/2024

---
+++ lesson26/Lap5_01/Lap05_01/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Lap05_01
+{
+    /// <summary>
+    /// Mội số thao tác với mảng 1 chièu
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding=System.Text.Encoding.UTF8;
+            // khai báo và khỏi tạo mảng 1 chiều
+            //int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] array = { 1, 2, 3,2,1};
+            //duyệt mảng và in ra
+            Console.WriteLine("các phần tử của mảng");
+            for (int i = 0; i < array.Length; i++) { 
+                Console.Write("{0}  ,", array[i]);
+            }
+            //tìm phần tửu lớn nhất
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++) {
+                if(max < array[i])
+                { 
+                    max= array[i];
+                }
+               
+            }
+            Console.WriteLine("\nPhần tử lớn nhất trong mảng là {0}",max);
+            //kiểm tra đổi xứng
+            bool kt=true;//giả sử mảng là đối xứng
+            int count=array.Length;
+            for (int i = 0; i < count; i++) {
+                if (array[i] != array[count - i - 1])
+                {
+                    kt = false;
+                    break;
+                } 
+            }
+            if (kt)
+            {
+                Console.WriteLine("Mảng là đối xứng");
+            }
+            else { Console.WriteLine("Mảng là không đối xứng");
+            }
+        }
+    }
+}
---
+++ lesson26/Lap5_01/Lap05_02/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Lap05_02
+{
+    /// <summary>
+    /// mảng 2 chiều
+    /// khởi tạo mảng 2 chiều 4x3
+    /// duyệt mảng và in theo ma trận
+    /// in ra các phần tử có chỉ số hàng bằng chỉ số cột
+    /// in ra phần tử lớn nhất của mỗi hàng
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding=System.Text.Encoding.UTF8;
+            Console.WriteLine("mảng 2 chiều");
+            int[,] array =
+            {
+                {4,6,9 },
+                {2,4,5 },
+                {9,2,6 },
+                {1,6,3 }
+            };
+            //duyệt mảng và in ra
+            printArray(array);
+            //tìm các phần tử cố số hàng bằng cột
+            printArrayXY(array);
+            //tìm các phần tửu lớn nhất trên hàng
+            printArrayMax(array);
+        }
+        static void printArray(int[,] array) {
+            Console.WriteLine("các phần tử trong mảng\n");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(" {0} ",array[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void printArrayXY(int[,] array)
+        {
+            //in ra phần tử có số hàng bằng cột
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j=0; j < array.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(" {0} ", array[i,j]);
+                    }
+                    else
+                    {
+                        Console.Write("{0}", "   ");
+                    }
+                   
+                } Console.WriteLine() ;
+            }
+        }
+        /// <summary>
+        /// Các phần tửu lớn nhất trên mỗi hàng
+        /// </summary>
+        /// <param name="array"></param>
+        static void printArrayMax(int[,] array)
+        {
+            // in ra phần tử lớn nhất của hàng
+            for(int i=0; i < array.GetLength(0); i++)
+            {
+                int max = array [i,0];
+                for(int j=1 ; j < array.GetLength(1); j++)
+                {
+                    if(max< array[i, j])
+                    {
+                        max= array [i,j];
+                    }
+                }
+                Console.WriteLine("\nPhần tử lớn nhất hàng {0} là :{1}",i,max);
+            }
+        }
+    }
+}
