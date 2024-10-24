d4c57aaf44edb5aeaac1b335ced01cefe7f94f3d
Nguyễn Văn An <vanan9524@gmail.com>
2024-10-24 22:38:20 +0700

    thứ 5 24/10/2024

---
+++ lesson28/Ex01/Program.cs
@@ -0,0 +1,26 @@
+﻿using DongVat.AnCo;
+using static Ex01.AnThit;
+
+namespace DongVat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bo bo = new Bo(1, "Bo", 500);
+            Trau trau = new(2, "Trau", 600);
+            De de = new(3, "De", 60);
+
+            CaSau caSau = new(4, "CaSau", 100);
+            Ho ho = new(5, "Ho", 60);
+            SuTu suTu = new(6, "SuTu", 45);
+            Console.WriteLine(bo.GetInfo());
+            Console.WriteLine(trau.GetInfo());
+            Console.WriteLine(de.GetInfo());
+            Console.WriteLine(ho.GetInfo());
+            Console.WriteLine(suTu.GetInfo());
+            Console.WriteLine(caSau.GetInfo());
+        }
+        
+    }
+}
---
+++ lesson28/Lap07_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lap07_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
